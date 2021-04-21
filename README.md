e3ae0eecf71d73199bf1b02f71b4e9c957b5fe13
Aslihan Akbiyik <aslihana@yandex.com>
2021-04-21 18:43:48 +0300

    upload files

---
+++ README.md
@@ -0,0 +1,2 @@
+# test-av
+Test repo for file scan written in C++ with QT.
---
+++ singlescanthread.h
@@ -0,0 +1,39 @@
+#ifndef SINGLESCANTHREAD_H
+#define SINGLESCANTHREAD_H
+
+#include <QThread>
+#include <QObject>
+#include <QFileDialog>
+#include <QStringListIterator>
+
+class SingleScanThread : public QThread
+
+{
+    Q_OBJECT
+
+public:
+    SingleScanThread(QStringList, QString, QString, QStringList, QThread *parent = 0);
+    void run();
+    bool stopSingleThread;
+
+signals:
+    void scanStart();
+    void scanComplete();
+    void infectedFiles(QString);
+
+public slots:
+
+private:
+    QStringList virusList;
+    QString passFileToScan;
+    QStringList alist;
+    QString location;
+    QStringList hashValues;
+    int hashType;
+
+};
+
+#endif // SINGLESCANTHREAD_H
+
+
+
